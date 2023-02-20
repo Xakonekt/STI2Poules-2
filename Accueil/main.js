560b43f0052c3a3c45fc57d9e353ae42383a7372
Xakonekt <97961930+Xakonekt@users.noreply.github.com>
2023-02-21 00:28:30 +0100

    Add files via upload

---
+++ Accueil/main.js
@@ -0,0 +1,11 @@
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+});
+
+$(document).on("click", ".onglet-side a", function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
